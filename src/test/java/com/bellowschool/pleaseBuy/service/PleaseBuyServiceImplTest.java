7e6a75c66fceba91ab3a840382dade630ac49319
rewq985 <bnbt3@naver.com>
2021-07-22 21:16:37 +0900

    [bugfix] test 코드 오류 수정

--- src/test/java/com/bellowschool/pleaseBuy/service/PleaseBuyServiceImplTest.java
+++ src/test/java/com/bellowschool/pleaseBuy/service/PleaseBuyServiceImplTest.java
@@ -1,25 +1,25 @@
 package com.bellowschool.pleaseBuy.service;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootTest
-@RequiredArgsConstructor
 public class PleaseBuyServiceImplTest {
-    private final PleaseBuyService pleaseBuyService;
+
+    @Autowired
+    private PleaseBuyServiceImpl pleaseBuyService;
 
     @Test
     public void test() {
         Map<String, Object> params = new HashMap<>();
-        params.put("goodstype", '0');
-        params.put("goods", "치킨");
-        params.put("goodscount", 2);
-        params.put("goodsprice", 2);
+        params.put("type", '0');
+        params.put("name", "치킨");
+        params.put("amount", 2);
+        params.put("price", 2);
         params.put("usernum", '0');
         params.put("option", "없음");
         pleaseBuyService.regPleaseBuy(params);
